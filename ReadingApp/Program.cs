84b6ed782a7c1bcee9efebb0dee3f5680d2315c6
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-09-14 21:08:18 +0200

    Install Blazorise.Bootstrap, delete tailwind config

--- ReadingApp/Program.cs
+++ ReadingApp/Program.cs
@@ -1,8 +1,17 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ReadingApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services
+    .AddBlazorise(options => { options.Immediate = true; })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
